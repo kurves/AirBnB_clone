02f0c542bdd0bdf78c69575ed9eac653ec36dcc3
kurves <kavssimply@gmail.com>
2024-03-06 04:26:21 -0800

    class definition of base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,10 @@
-class BaseModel():
+#!/usr/bin/python3
+
+"""Define a class that instanitiate a new instance"""
+
+class BaseModel:
+    """base class that defines all attributes"""
+    def __init__():
     id = str(uuid.uuid4())
     created_at = datetime.now()
 
