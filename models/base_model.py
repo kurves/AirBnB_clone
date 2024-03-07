be95ec72f068620cfac812e949ddf07ccfaed826
kurves <kavssimply@gmail.com>
2024-03-07 05:59:08 -0800

    Dimport required modules

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+import uuid
+from datetime import datetime
+
 """Define a class that instanitiate a new instance"""
 
 class BaseModel:
     """base class that defines all attributes"""
-    def __init__():
+    def __init__(self):
     id = str(uuid.uuid4())
     created_at = datetime.now()
 
