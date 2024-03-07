a57dae5c1f2ec0869f4c709802e4bf2d07d43260
kurves <kavssimply@gmail.com>
2024-03-07 06:08:43 -0800

    sstr representation for class

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,16 @@ from datetime import datetime
 class BaseModel:
     """base class that defines all attributes"""
     def __init__(self):
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
+        """instantiate class atrributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """str definition for class"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__) 
+
+
+
 
