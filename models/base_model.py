935b26b583a606b8937737ae981c567f2d4dff6c
kurves <kavssimply@gmail.com>
2024-03-07 23:17:56 -0800

    function to set class dict

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,16 @@ from datetime import datetime
 
 class BaseModel:
     """base class that defines all attributes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """instantiate class atrributes"""
+        if kwargs:
+            for k,v in kwargs.items():
+                if k != '__class__':
+                    if k == 'created_at' or k == 'updated_at':
+                        value = datetime.striptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(sself, k, v)
+
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
