db9186327402e1a74fd7ce86a2d48fd331ac23cf
kurves <kavssimply@gmail.com>
2024-03-08 00:00:17 -0800

    instatiate cladd atributes

--- models/base_model.py
+++ models/base_model.py
@@ -14,19 +14,18 @@ class BaseModel:
                 if k != '__class__':
                     if k == 'created_at' or k == 'updated_at':
                         value = datetime.striptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(sself, k, v)
+                    setattr(self, k, v)
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
             if 'created_at' not in kwargs:
-                self.id = datetime.now()
+                self.created_at = datetime.now()
             if 'updated_at' not in kwargs:
                 self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
     def __str__(self):
         """str definition for class"""
