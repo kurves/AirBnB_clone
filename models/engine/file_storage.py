a031b2bc1d9a3b80ac939d6a978a79e3e6400144
kurves <kavssimply@gmail.com>
2024-03-09 08:40:20 -0800

    serialize objects data

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,29 +10,24 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
     
-    def __init__(self, file_path=None):
-        """Initializes the FileStorage with an optional file path."""
-        if file_path:
-            self.__file_path = file_path
-
-        self.reload()
-
-
     def all(self):
         """Return the objects."""
         return self.__objects
 
     def new(self, obj):
         """objects the obj with key <obj class name>.id."""
-        k = f"{type(obj).__name__}.{obj.id}"
+        k = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[k] = obj
 
     
     def save(self):
         """Serialize to the JSON file."""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
         try:
-            with open(self.__file_path, "w") as f:
-                json.dump(self.__objects, f, default=self.encode)
+            with open(self.__file_path, 'w') as f:
+                json.dump(serialized_objects, f)
         except Exception as e:
             print(f"Error saving to file: {e}")
 
@@ -40,17 +35,17 @@ class FileStorage:
         """Deserialize the JSON file"""
         try:
             with open(self.__file_path, 'r') as f:
-                self.__objects = json.load(f, object_hook=self.decodes)
+                self.__objects = json.load(f, object_hook=self.decode)
         except FileNotFoundError:
             pass
 
     def encode(self, obj):
         """Customizes JSON serialization for BaseModel objects."""
-        if isinstance(obj, BaseModel)
+        if isinstance(obj, BaseModel):
             return obj.to_dict()
         return obj
 
-     def decode(self, obj):
+    def decode(self, obj):
         """Customizes JSON deserialization for BaseModel objects."""
         if "__class__" in obj and obj["__class__"] == "BaseModel":
             from models.base_model import BaseModel
