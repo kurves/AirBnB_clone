4813f21d6f63f90f510483471bbd818163c3249b
kurves <kavssimply@gmail.com>
2024-03-10 00:21:19 -0800

    update function keywordsd

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """instantiate class atrributes"""
         if kwargs:
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
