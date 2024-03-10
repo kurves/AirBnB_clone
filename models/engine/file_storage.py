d70f546ca41e70d621ceed4a65baf945d8f5541f
kurves <kavssimply@gmail.com>
2024-03-10 01:31:01 -0800

    fix importation error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,7 +40,7 @@ class FileStorage:
                 for key, value in serialized_objects.items():
                     class_name = value.pop('__class__')
                     try:
-                        module = importlib.import_module(f'models.{class_name.lower()}')
+                        module = importlib.import_module(f'models.{class_name}')
                     except ImportError:
                         print(f"Error loading module '{class_name}'")
                         continue  # Skip to the next object
